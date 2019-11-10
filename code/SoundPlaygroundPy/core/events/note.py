dda56b2bd501d12bd7ad00774ba6ff2212c562e4
Pedro M. Silva <pemiolsi@hotmail.com>
2019-11-10 15:41:09 +0000

    Fix disabling future notes

--- code/SoundPlaygroundPy/core/events/note.py
+++ code/SoundPlaygroundPy/core/events/note.py
@@ -5,14 +5,31 @@ from fractions import Fraction
 from typing import Dict
 
 class NoteOnEvent ( MusicEvent ):
-    def __init__ ( self, timestamp = 0, pitch_class = 0, octave = 4, accidental = NoteAccidental.NONE, velocity = 0, channel = 0 ):
+    def __init__ ( self, timestamp = 0, pitch_class = 0, octave = 4, accidental = NoteAccidental.NONE, velocity = 0, channel = 0, parent : 'NoteEvent' = None ):
         super().__init__( timestamp )
 
         self.pitch_class : int = pitch_class
         self.octave : int = octave
         self.accidental : int = accidental
         self.velocity : int = velocity
-        self.channel = channel
+        self.channel : int = channel
+        self.parent : 'NoteEvent' = parent
+
+        self._disabled : bool = False
+
+    @property
+    def disabled ( self ) -> bool:
+        if self.parent != None:
+            return self.parent.disabled
+        
+        return self._disabled
+
+    @disabled.setter
+    def disabled ( self, value : bool ):
+        if self.parent != None:
+            self.parent.disabled = value
+        else:
+            self._disabled = value
 
     @property
     def note ( self ) -> Note:
@@ -26,13 +43,28 @@ class NoteOnEvent ( MusicEvent ):
 
 
 class NoteOffEvent ( MusicEvent ):
-    def __init__ ( self, timestamp = 0, pitch_class = 0, octave = 4, accidental = NoteAccidental.NONE, channel = 0 ):
+    def __init__ ( self, timestamp = 0, pitch_class = 0, octave = 4, accidental = NoteAccidental.NONE, channel = 0, parent : 'NoteEvent' = None ):
         super().__init__( timestamp )
 
         self.pitch_class : int = pitch_class
         self.octave : int = octave
         self.accidental : int = accidental
-        self.channel = channel
+        self.channel : int = channel
+        self.parent : 'NoteEvent' = parent
+
+    @property
+    def disabled ( self ) -> bool:
+        if self.parent != None:
+            return self.parent.disabled
+        
+        return self._disabled
+
+    @disabled.setter
+    def disabled ( self, value : bool ):
+        if self.parent != None:
+            self.parent.disabled = value
+        else:
+            self._disabled = value
 
     @property
     def note ( self ) -> Note:
@@ -59,11 +91,11 @@ class NoteEvent( DurationEvent ):
 
     @property
     def note_on ( self ) -> NoteOnEvent:
-        return NoteOnEvent( self.timestamp, self.pitch_class, self.octave, self.accidental, self.velocity, self.channel )
+        return NoteOnEvent( self.timestamp, self.pitch_class, self.octave, self.accidental, self.velocity, self.channel, parent = self )
 
     @property
     def note_off ( self ) -> NoteOnEvent:
-        return NoteOffEvent( self.timestamp + self.duration, self.pitch_class, self.octave, self.accidental, self.channel )
+        return NoteOffEvent( self.timestamp + self.duration, self.pitch_class, self.octave, self.accidental, self.channel, parent = self )
 
     def __int__ ( self ):
         return int( self.note )
