39dd0725f27da1cdd0a3977a9f6edd55f469d6c0
Pedro M. Silva <pemiolsi@hotmail.com>
2020-06-09 19:57:06 +0100

    Begin writing final dissertation

---
+++ dissertation/final/build-diagrams.sh
@@ -0,0 +1,3 @@
+mmdc --theme "neutral" -p "img/puppeteer-config.json" -i img/diagram_architecture.mmd -o img/diagram_architecture.png
+mmdc --theme "neutral" -p "img/puppeteer-config.json" -i img/diagram_virtualmachine.mmd -o img/diagram_virtualmachine.png
+mmdc --theme "neutral" -p "img/puppeteer-config.json" -i img/diagram_calendarization.mmd -o img/diagram_calendarization.png --cssFile img/diagram_calendarization.css
